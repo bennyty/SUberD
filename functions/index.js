7bceaefbc0a420b1977f5d8e309a845862d96bd2
Tristan Wise <Twise78738@union-wl-316.dynamic2.rpi.edu>
2017-03-26 11:03:03 -0400

    firebase functions initialization

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+var functions = require('firebase-functions');
+
+//functions to manage adding and removing data after 24 hours, or dailey at a certain time.
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// })
