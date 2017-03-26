04f2273ffdafa9523b7141545eb7b554f18c5f45
tjpassaro <tjpassaro@gmail.com>
2017-03-26 17:42:46 -0400

    updated index.js with request ride

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Router, Scene} from 'react-native-router-flux'
 import {Provider, connect} from 'react-redux'
 import configureStore from './config/store/configureStore'
+import RequestRide from './layouts/RequestRide'
 
 const RouterWithRedux = connect()(Router)
 const store = configureStore()
@@ -11,8 +12,8 @@ export default class App extends Component {
     return (
       <Provider store={store}>
         <RouterWithRedux>
-          // <Scene key='login' component={Login} title='Login Page' />
-          // <Scene key='loginTwo' component={Login} title='Login Two' />
+          <Scene key='login' component={RequestRide} title='Login Page' />
+          <Scene key='loginTwo' component={RequestRide} title='Login Two' />
         </RouterWithRedux>
       </Provider>
     )
