48b923744e6c2bfaa9ba03f9ff2cdb4a42e63a6e
tjpassaro <tjpassaro@gmail.com>
2017-04-20 18:03:03 -0400

    a

--- app/layouts/RideConfirmation/VisibleRideConfirmation.js
+++ app/layouts/RideConfirmation/VisibleRideConfirmation.js
@@ -2,14 +2,16 @@ import { connect } from 'react-redux'
 import RideConfirmation from './RideConfirmation'
 import Reactotron from 'reactotron-react-native'
 import {Actions} from 'react-native-router-flux'
+import {requestRide} from '../../actions'
 
 const mapDispatchToProps = (dispatch) => {
   return {
   	//Is called by the RequestRide class on the 'submit' button press
     onConfirmClick: (eventID, phoneNumber, name, pickup, dropoff, numRiders, comment) => {
+    	Reactotron.log(JSON.stringify(dispatch))
+    	dispatch(requestRide({eventID, phoneNumber: "6312523291", name: "TJ", pickup, dropoff, numRiders, comment, status: 1}));
     	alert("Submitted Ride Request");
-      //dispatch(requestRide({eventId, phoneNumber, name, pickup, dropoff, numRiders, comment}));
-      Actions.wait({eventID: eventID, phoneNumber: phoneNumber, name: name, pickup: pickup, dropoff: dropoff, numRiders: numRiders, comment: comment});
+      	Actions.wait({eventID: eventID, phoneNumber: phoneNumber, name: name, pickup: pickup, dropoff: dropoff, numRiders: numRiders, comment: comment});
     }
   }
 }
@@ -17,7 +19,7 @@ const mapDispatchToProps = (dispatch) => {
 //Connects the Ride Request page to a dispatcher which
 //can send actions out via the Factory
 const VisibleRideConfirmation = connect(
-  mapDispatchToProps
+  null, mapDispatchToProps
 )(RideConfirmation)
 
 export default VisibleRideConfirmation
